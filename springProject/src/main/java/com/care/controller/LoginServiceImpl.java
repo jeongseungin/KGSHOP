fac9b3206a3279c669166090ef00fb44ff6c93a5
aaa.aaa <aaa@aaa.aaa>
2020-05-20 20:59:05 +0900

    로그인

---
+++ springProject/src/main/java/com/care/controller/LoginServiceImpl.java
@@ -0,0 +1,15 @@
+package com.care.controller;
+
+import org.springframework.ui.Model;
+
+import com.care.service.CommonService;
+
+public class LoginServiceImpl implements CommonService {
+
+
+	public int execute(Model model) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
